3ac7ed6a291ef71d6eadd359d307d681925beaf8
retired exploiter <alex.maeots14@gmail.com>
2026-07-05 20:50:23 +0100

    https://drive.google.com/drive/folders/1VlagpCHIEqCpRr-GxC75yrEs8feWPz7q?usp=drive_link

---
+++ dataset/bad/mine/bad11.lua
@@ -0,0 +1,6 @@
+local high = 1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11*1e11
+
+for i = 1, high do
+  print("they will never remember your face")
+  warn("they will never remember you")
+end
