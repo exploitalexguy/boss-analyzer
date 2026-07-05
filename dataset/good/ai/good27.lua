944c67a54c2bea78e72f8a23bf2018d600aea919
retired exploiter <alex.maeots14@gmail.com>
2026-07-05 22:23:18 +0100

    fix low priority issue: word "hack" is recognised as bad

---
+++ dataset/good/ai/good27.lua
@@ -0,0 +1,9 @@
+local mouse = game.Players.LocalPlayer:GetMouse()
+local marker = Instance.new("Part")
+marker.Size = Vector3.new(1,1,1)
+marker.Anchored = true
+marker.Name = "HackMarker"
+marker.CFrame = CFrame.new(mouse.Hit.Position)
+marker.Parent = workspace
+
+game:GetService("Debris"):AddItem(marker, 0.2)
